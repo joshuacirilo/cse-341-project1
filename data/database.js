db111c6ada8f8f3166b06bb1abeb0d44d18015ee
joshuacirilo <u175692795@byui.edu>
2026-07-18 22:46:34 -0600

    Use environment MongoDB URI

--- data/database.js
+++ data/database.js
@@ -5,35 +5,13 @@ const MongoClient = require('mongodb').MongoClient;
 
 let database;
 
-const getMongoUri = () => {
-  const mongoUri = process.env.MONGODB_URI || process.env.MONGODB_URL;
-
-  if (!mongoUri) {
-    return null;
-  }
-
-  const parsedUri = new URL(mongoUri);
-
-  if (parsedUri.protocol !== 'mongodb+srv:' || parsedUri.host !== 'cluster0.qsm49cb.mongodb.net') {
-    return mongoUri;
-  }
-
-  const hosts = [
-    'ac-rfonwom-shard-00-00.qsm49cb.mongodb.net:27017',
-    'ac-rfonwom-shard-00-01.qsm49cb.mongodb.net:27017',
-    'ac-rfonwom-shard-00-02.qsm49cb.mongodb.net:27017'
-  ].join(',');
-
-  return `mongodb://${parsedUri.username}:${parsedUri.password}@${hosts}${parsedUri.pathname}?ssl=true&authSource=admin&retryWrites=true&w=majority`;
-};
-
 const intDb = (callback) => {
   if (database) {
     console.log('Db is already initialized!');
     return callback(null, database);
   }
 
-  const mongoUri = getMongoUri();
+  const mongoUri = process.env.MONGODB_URI || process.env.MONGODB_URL;
 
   if (!mongoUri) {
     return callback('Missing MONGODB_URI environment variable');
